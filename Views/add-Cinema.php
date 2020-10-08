11371e10c185855aef6e1d5f12a524f8f643a62b
isaiasemanuel22 <isaiasemnuelcalfin@hotmail.com>
2020-10-07 21:03:13 -0300

    agregado vistas add cinema

---
+++ Views/add-Cinema.php
@@ -0,0 +1,23 @@
+<section class="Add-cinema">
+
+    <div class="grid-content-add-cinema">
+
+        <div class="content-add-cimena">
+            <div class="content-form">
+                <h1 class="tittle-add-cinema">Add Cinema</h1>
+                <form action="" class="form-add-cinema">
+                    <div class="input-cinema"><span><b>Nombre de cine:</b> </span><input type="text" name="" id=""></div>
+                    <div class="input-cinema"><span><b>Direccion de cine:</b> </span><input type="text" name="" id=""></div>
+                    <div class="input-cinema"><span><b>Telfono:</b> </span><input type="text" name="" id=""></div>
+                    <div class="input-cinema"><span><b>Cantidad de Salas:</b> </span><input type="num" name="" id=""></div>
+                    
+                    <button class="submit-add-cinema" type="submit">Agregar</button>
+
+                </form>
+            </div>
+
+
+        </div>
+    </div>
+
+</section>
--- Views/header.php
+++ Views/header.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="<?php echo CSS_PATH; ?>login.css">	
     <link rel="stylesheet" href="<?php echo CSS_PATH; ?>nav.css">
     <link rel="stylesheet" href="<?php echo CSS_PATH; ?>Home.css">
+    <link rel="stylesheet" href="<?php echo CSS_PATH; ?>add-MovieThreater.css">
     <link rel="stylesheet" type="text/css" href="<?php echo SLICK; ?>/slick.css"/>
     <link rel="stylesheet" type="text/css" href="<?php echo SLICK; ?>/slick-theme.css"/>    
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
