9bc96b53d89c55b32ec8416bbcfd0d261d6a84dc
isaiasemanuel22 <isaiasemnuelcalfin@hotmail.com>
2020-10-14 09:59:20 -0300

    actulizacion del Home.css

--- Views/header.php
+++ Views/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Multiflex</title>
     <link rel="stylesheet" href="<?php echo CSS_PATH; ?>login.css">	
-    <!--link rel="stylesheet" href="<?php echo CSS_PATH; ?>nav.css"-->
+
     <link rel="stylesheet" href="<?php echo CSS_PATH; ?>add-Room.css">
     <link rel="stylesheet" href="<?php echo CSS_PATH; ?>add-MovieThreater.css">
     <link rel="stylesheet" href="<?php echo CSS_PATH; ?>Home.css">
