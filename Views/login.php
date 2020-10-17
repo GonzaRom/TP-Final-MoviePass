88bcce1f746c446406024db21f7b7cdb23093eba
isaiasemanuel22 <isaiasemnuelcalfin@hotmail.com>
2020-10-16 21:27:22 -0300

    actualizacion de estilos(home , list-cinema , list-movies , login ) y accesos (sign-in)

--- Views/login.php
+++ Views/login.php
@@ -4,9 +4,9 @@
             <a href="<?php echo FRONT_ROOT;?>Home/Index"><img class="logo-img" src="<?php echo IMG_PATH; ?>/multiflex.png" alt=""></a>
 
             <p class="Tittle">Login</p>
-            <form class="form-login" action="<?php echo FRONT_ROOT;?>User/login" method="POST">
-                <input class="input-login" type="text" name="userName" id="" placeholder="User Name">
-                <input class="input-login" type="password" name="password" id="" placeholder="Password">
+            <form class="form-login" action="<?php echo FRONT_ROOT;?>User/login" method="POST"  >
+                <input class="input-login" type="text" name="userName" id="" placeholder="User Name" required>
+                <input class="input-login" type="password" name="password" id="" placeholder="Password" required>
                 <button class="submit-login" type="submit">Login</button>
             </form>
             <div class="enlace">
--- Views/sign-in.php
+++ Views/sign-in.php
@@ -1,32 +1,36 @@
-
-<div class="content-body">
-    <?php if(isset($_SESSION['loggedUser'])){
-        require_once("nav.php");   
-    }
+<div class="vhSignIn">       
+ <?php if (isset($_SESSION['loggedUser'])) {
+            require_once("nav.php");
+        }
         ?>
-    <section class="login">
-        <div class="content-login">
-            <img class="logo-img" src="<?php echo IMG_PATH; ?>/multiflex.png" alt="">
+    <div class="content-body">
 
-            <p class="Tittle">Sign In</p>
-            <form class="form-login" action="<?php echo FRONT_ROOT;?>User/signIn" method="POST">
-                <input class="input-login" type="text" name="firtsName" placeholder="First Name" required>
-                <input class="input-login" type="text" name="lastName" placeholder="Last Name" required>
-                <input class="input-login" type="text" name="userName" placeholder="User Name" required>
-                <input class="input-login" type="email" name="email" placeholder="Email" required>
-                <input class="input-login" type="password" name="password" placeholder="Password" required>
-                <?php if(!empty($_SESSION['userType']) && $_SESSION['userType'] == 2):?>
-                <select name="userType" id="userType">
-                    <?php foreach($userTypeList as $userType):  ?>
-                        <option value="<?php echo $userType->getId(); ?>" id="userType"><?php echo $userType->getName(); ?></option>
-                    <?php endforeach; ?>
-                </select>
+        <section class="login">
+            <div class="content-login">
+                <img class="logo-img" src="<?php echo IMG_PATH; ?>/multiflex.png" alt="">
+
+                <p class="Tittle">Sign In</p>
+                <form class="form-login" action="<?php echo FRONT_ROOT; ?>User/signIn" method="POST">
+                    <input class="input-login" type="text" name="firtsName" placeholder="First Name" required>
+                    <input class="input-login" type="text" name="lastName" placeholder="Last Name" required>
+                    <input class="input-login" type="text" name="userName" placeholder="User Name" required>
+                    <input class="input-login" type="email" name="email" placeholder="Email" required>
+                    <input class="input-login" type="password" name="password" placeholder="Password" required>
+                    <?php if (!empty($_SESSION['userType']) && $_SESSION['userType'] == 2) : ?>
+                        <select name="userType" id="userType">
+                            <?php foreach ($userTypeList as $userType) :  ?>
+                                <option value="<?php echo $userType->getId(); ?>" id="userType"><?php echo $userType->getName(); ?></option>
+                            <?php endforeach; ?>
+                        </select>
                     <?php endif; ?>
-                <button class="submit-login" type="submit">Sign in</button>
-            </form>
-            <div class="enlace">
-                <a href="<?php echo FRONT_ROOT; ?>User/showLoginView"><b>Login</b></a>
+                    <button class="submit-login" type="submit">Sign in</button>
+                </form>
+                <?php if (!isset($_SESSION['loggedUser'])):?>
+                <div class="enlace">
+                    <a href="<?php echo FRONT_ROOT; ?>User/showLoginView"><b>Login</b></a>
+                </div>
+                <?php endif;?>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 </div>
