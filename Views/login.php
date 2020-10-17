575eac36482bbd2ca7c49e21dd6c412d0ab9f493
gonzalorom <gromero@makingsense.com>
2020-10-17 18:38:52 -0300

    login y sign in en español

--- Views/login.php
+++ Views/login.php
@@ -1,19 +1,19 @@
 <div class="content-body">
     <section class="login">
         <div class="content-login">
-            <a href="<?php echo FRONT_ROOT;?>Home/Index"><img class="logo-img" src="<?php echo IMG_PATH; ?>/multiflex.png" alt=""></a>
+            <a href="<?php echo FRONT_ROOT; ?>Home/Index"><img class="logo-img" src="<?php echo IMG_PATH; ?>/multiflex.png" alt=""></a>
 
-            <p class="Tittle">Login</p>
-            <form class="form-login" action="<?php echo FRONT_ROOT;?>User/login" method="POST">
-                <input class="input-login" type="text" name="userName" id="" placeholder="User Name">
+            <p class="Tittle">Ingresar</p>
+            <form class="form-login" action="<?php echo FRONT_ROOT; ?>User/login" method="POST">
+                <input class="input-login" type="text" name="userName" id="" placeholder="Nombre de usuario">
                 <input class="input-login" type="password" name="password" id="" placeholder="Password">
-                <button class="submit-login" type="submit">Login</button>
+                <button class="submit-login" type="submit">Ingresar</button>
             </form>
             <div class="enlace">
-                <a href="<?php echo FRONT_ROOT; ?>User/showSingInView"><b>Sign Up</b></a>
+                <a href="<?php echo FRONT_ROOT; ?>User/showSingInView"><b>Registrarse</b></a>
             </div>
 
-            <?php if($message != "" ):?>
+            <?php if ($message != "") : ?>
                 <p style="color: #fff;"><?php echo $message; ?></p>
             <?php endif; ?>
         </div>
--- Views/sign-in.php
+++ Views/sign-in.php
@@ -8,11 +8,11 @@
         <div class="content-login">
             <img class="logo-img" src="<?php echo IMG_PATH; ?>/multiflex.png" alt="">
 
-            <p class="Tittle">Sign In</p>
+            <p class="Tittle">Registrarse</p>
             <form class="form-login" action="<?php echo FRONT_ROOT;?>User/signIn" method="POST">
-                <input class="input-login" type="text" name="firtsName" placeholder="First Name">
-                <input class="input-login" type="text" name="lastName" placeholder="Last Name">
-                <input class="input-login" type="text" name="userName" placeholder="User Name">
+                <input class="input-login" type="text" name="firtsName" placeholder="Nombre">
+                <input class="input-login" type="text" name="lastName" placeholder="Apellido">
+                <input class="input-login" type="text" name="userName" placeholder="Nombre de usuario">
                 <input class="input-login" type="email" name="email" placeholder="Email">
                 <input class="input-login" type="password" name="password" placeholder="Password">
                 <?php if(!empty($_SESSION['userType']) && $_SESSION['userType'] == 2):?>
@@ -22,7 +22,7 @@
                     <?php endforeach; ?>
                 </select>
                     <?php endif; ?>
-                <button class="submit-login" type="submit">Sign in</button>
+                <button class="submit-login" type="submit">Ingresar</button>
             </form>
             <div class="enlace">
                 <a href="<?php echo FRONT_ROOT; ?>User/showLoginView"><b>Login</b></a>
