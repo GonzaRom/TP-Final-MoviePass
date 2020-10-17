9d48808ed73d6044ebd53644383c0359880a0469
isaiasemanuel22 <isaiasemnuelcalfin@hotmail.com>
2020-10-16 21:32:08 -0300

    AHORA SI JEJE actualizacion vista login

--- Views/login.php
+++ Views/login.php
@@ -1,21 +1,23 @@
-<div class="content-body">
-    <section class="login">
-        <div class="content-login">
-            <a href="<?php echo FRONT_ROOT;?>Home/Index"><img class="logo-img" src="<?php echo IMG_PATH; ?>/multiflex.png" alt=""></a>
+<div class="vhSignIn">
+    <div class="content-body">
+        <section class="login">
+            <div class="content-login">
+                <a href="<?php echo FRONT_ROOT; ?>Home/Index"><img class="logo-img" src="<?php echo IMG_PATH; ?>/multiflex.png" alt=""></a>
 
-            <p class="Tittle">Login</p>
-            <form class="form-login" action="<?php echo FRONT_ROOT;?>User/login" method="POST"  >
-                <input class="input-login" type="text" name="userName" id="" placeholder="User Name" required>
-                <input class="input-login" type="password" name="password" id="" placeholder="Password" required>
-                <button class="submit-login" type="submit">Login</button>
-            </form>
-            <div class="enlace">
-                <a href="<?php echo FRONT_ROOT; ?>User/showSingInView"><b>Sign Up</b></a>
+                <p class="Tittle">Login</p>
+                <form class="form-login" action="<?php echo FRONT_ROOT; ?>User/login" method="POST">
+                    <input class="input-login" type="text" name="userName" id="" placeholder="User Name" required>
+                    <input class="input-login" type="password" name="password" id="" placeholder="Password" required>
+                    <button class="submit-login" type="submit">Login</button>
+                </form>
+                <div class="enlace">
+                    <a href="<?php echo FRONT_ROOT; ?>User/showSingInView"><b>Sign Up</b></a>
+                </div>
+
+                <?php if ($message != "") : ?>
+                    <p style="color: #fff;"><?php echo $message; ?></p>
+                <?php endif; ?>
             </div>
-
-            <?php if($message != "" ):?>
-                <p style="color: #fff;"><?php echo $message; ?></p>
-            <?php endif; ?>
-        </div>
-    </section>
+        </section>
+    </div>
 </div>
