fc9e9ec3f29115c766538427849ceaaf5e5ee610
isaiasemanuel22 <isaiasemnuelcalfin@hotmail.com>
2020-10-21 09:50:15 -0300

    creacion parcial ticket

---
+++ DAO/ITicketDAO.php
@@ -0,0 +1,12 @@
+<?php namespace DAO;
+
+use Models\Ticket;
+
+
+
+interface ITicketDAO{
+    public function add(Ticket $ticket);
+    public function getAll();
+    public function get($id);
+}
+?>
